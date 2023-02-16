28be237a5eaa03687c71c1f0c36a9303f27f8272
Cod3guy <ian.kipkorir0799@gmal.com>
2023-02-16 12:00:31 -0800

    4-puts.c Did some few changes to the file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,8 +4,10 @@
  *
  * Return: Always 0 (success)
  */
-int main(void)
+int main(void) /* THis is the main function */
 {
+	/*We display the line */
 	puts("\"Programming is like building a multilingual puzzle");
+	/*The return code for the main function */
 	return (0);
 }
