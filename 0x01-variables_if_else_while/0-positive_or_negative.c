268597e707d04b1fccb04f65e7d77980f8077b36
Cod3guy <ian.kipkorir0799@gmal.com>
2023-02-17 07:08:37 -0800

    Msg--------->"Edited it to be in betty coding style"

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,9 +3,11 @@
 #include <time.h>
 
 /**
- *  more headers goes there 
- *
- *  betty style doc for function main goes there 
+ * main - Entry point
+ * 
+ * Description: 'the program's description'
+ * 
+ * Return: Always 0 (Success)
  */
 
 int main(void)
