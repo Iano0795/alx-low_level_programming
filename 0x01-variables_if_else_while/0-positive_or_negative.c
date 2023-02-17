82346dc28a6fd423979ef840d8134533d9f053a5
Cod3guy <ian.kipkorir0799@gmal.com>
2023-02-17 07:32:01 -0800

    Msg--------->"Edited it to be in betty coding style"

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,37 +3,32 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
- *
- * Description: This program generates a random integer and
- *              determines whether it is positive, negative, or zero.
+ * main -Entry point
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-        int n;
+	int n;
 
-        /* Seed the random number generator */
-        srand(time(0));
+	srand(time(0));
 
-        /* Generate a random integer between -RAND_MAX/2 and +RAND_MAX/2 */
-        n = rand() - RAND_MAX / 2;
+	n = rand() - RAND_MAX / 2;
 
-        /* Check if the number is positive, negative, or zero */
-        if (n > 0)
-        {
-                printf("%d is positive\n", n);
-        }
-        else if (n == 0)
-        {
-                printf("%d is zero\n", n);
-        }
-        else
-        {
-                printf("%d is negative\n", n);
-        }
+	/* your code goes here */
+	if (n > 0)
+	{
+		printf("%d is positive\n", n);
+	}
+	else if (n == 0)
+	{
+		printf("%d is zero\n", n);
+	}
+	else
+	{
+		printf("%d is negative\n", n);
+	}
 
-        return (0);
+	return (0);
 }
 
