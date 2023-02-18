184049fffd2a70930fbbecaec23b4f6e8b076fbc
Cod3guy <ian.kipkorir0799@gmal.com>
2023-02-18 06:09:30 -0800

    Msg--------->"added the last task"

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+/**
+ * main - Printing all combinations of a two two digit number
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int tens;
+	int ones;
+	int t;
+	int o;
+
+	for (tens = '0'; tens <= '9'; tens++)
+	{
+		for (ones = (tens + 1); ones <= '9'; ones++)
+		{
+			for (t = tens; t <= '9'; t++)
+			{
+				for (o = ones; o <= '9'; o++)
+				{
+					putchar (tens);
+					putchar (ones);
+					putchar (' ');
+					putchar (t);
+					putchar (o);
+					if (!((tens == '9' && ones == '8') && (t == '9' && o == '9')))
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar ('\n');
+	
+	return (0);
+}
