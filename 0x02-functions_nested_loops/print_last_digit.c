f2c8e1a9925ee5bd88ee4b39573a7ca08cb756cf
Cod3guy <ian.kipkorir0799@gmal.com>
2023-02-21 10:59:54 -0800

    Msg--------->"added"

---
+++ 0x02-functions_nested_loops/print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_last_digit - Prints the last digit of a number
+ * @l: character to be examined
+ * Return: returns a value
+ */
+int print_last_digit(int l)
+{
+	int i = l % 10;
+	if (i < 0)
+		i *= -1;
+	_putchar (i + '0');
+	return (i);
+}
