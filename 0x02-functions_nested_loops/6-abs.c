63c98589338e9462dc4ea14aa5d8fd38b4fe5887
Cod3guy <ian.kipkorir0799@gmal.com>
2023-02-21 10:51:11 -0800

    Msg--------->"added"

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs - absolute value of an integer
+ * @i: chracter to be verified
+ * Return: Returns a certain value
+ */
+int _abs(int)
+{
+	if (i > 0)
+		return (i);
+	else if (i < 0)
+		return (-i);
+	else
+		return (0);
+}
