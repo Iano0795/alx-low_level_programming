afe1d2ae949be67d98bcd5297ed2b5e0734be534
Cod3guy <ian.kipkorir0799@gmal.com>
2023-02-21 04:35:06 -0800

    Msg--------->"edited the first file"

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,22 @@
-#include <stdio.h>
+#include <unistd.h>
 
 /** 
  * main - Program to display _putchar
  *
  * Return: Always 0 (Success)
  */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
 int main(void)
 {
-	printf("_putchar\n");
-
+	char put[8] = "_putchar";
+	int i;
+	for (i = 0; i < put[i]; i++)
+	{
+		_putchar(put[i]);
+	}
+	_putchar('\n');
 	return (0);
 }
