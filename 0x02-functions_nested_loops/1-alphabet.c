4b21231410581156e46e27aa8cc127eb50b8b1af
Cod3guy <ian.kipkorir0799@gmal.com>
2023-02-21 05:51:30 -0800

    Msg--------->"added"

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,13 +1,14 @@
 #include "main.h"
 
 /**
- * main - program to print alphabet in lowercase
+ * print_alphabet - program to print alphabet in lowercase
  *
  * Return: Always 0 Success
  */
 void print_alphabet(void)
 {
 	char letter;
+
 	for (letter = 'a'; letter <= 'z'; letter++)
 	{
 		_putchar(letter);
