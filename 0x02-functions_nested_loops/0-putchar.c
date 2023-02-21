e3ebaaf6c01cc9fdc5e189aee580c7dc892dc696
Cod3guy <ian.kipkorir0799@gmal.com>
2023-02-21 04:45:26 -0800

    Msg--------->"added everything"

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,15 +1,10 @@
-#include <unistd.h>
+#include "main.h"
 
 /** 
  * main - Program to display _putchar
  *
  * Return: Always 0 (Success)
  */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
-
 int main(void)
 {
 	char put[8] = "_putchar";
