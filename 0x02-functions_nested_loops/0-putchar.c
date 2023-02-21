9d74e6321b9c5f9dd7e942d84d46f47fe439adea
Cod3guy <ian.kipkorir0799@gmal.com>
2023-02-21 05:17:48 -0800

    Msg--------->"added"

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,12 +7,15 @@
  */
 int main(void)
 {
-	char put[8] = "_putchar";
-	int i;
-	for (i = 0; i < put[i]; i++)
-	{
-		_putchar(put[i]);
-	}
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+
 	_putchar('\n');
 
 	return (0);
