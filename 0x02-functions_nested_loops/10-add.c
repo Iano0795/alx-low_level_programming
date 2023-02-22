9eff5b9d2847d7308d3e9477196fba9b9fdda19b
Cod3guy <ian.kipkorir0799@gmal.com>
2023-02-22 00:37:09 -0800

    Msg--------->"added"

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - addng two ints
+ * @a: first int
+ * @b: sec int
+ * Return: Always 0 for success
+ */
+int add(int a, int b)
+{
+	int c = a + b;
+
+	return (c);
+}
