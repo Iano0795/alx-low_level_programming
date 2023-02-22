f504cb320ac5fd13ef9f1c08f5391de87dffd8fe
Cod3guy <ian.kipkorir0799@gmal.com>
2023-02-22 00:23:53 -0800

    Msg--------->"added"

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -13,7 +13,7 @@ void jack_bauer(void)
 	{
 		for (min = 0; min <= 59; min++)
 		{
-			_putchar ((hour \ 10) + '0');
+			_putchar ((hour / 10) + '0');
 			_putchar ((hour % 10) + '0');
 			_putchar (':');
 			_putchar ((min / 10) + '0');
