4043c41a1b4134631cb2bb67539559211a8ed310
Cod3guy <ian.kipkorir0799@gmail.com>
2023-02-23 15:17:27 -0600

    More functionswq

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * isupper - checks for uppercase letters
+ *
+ * Return: void
+ */
+int isupper(char c)
+{
+	if (c = 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else 
+		return (0);
+}
