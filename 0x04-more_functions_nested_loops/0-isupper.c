09a38d55cdc1e3c7768409444cdfb945ec577551
Cod3guy <ian.kipkorir0799@gmail.com>
2023-02-23 22:24:48 -0600

    More functionswq

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,17 +2,13 @@
 
 /**
  * isupper - checks for uppercase letters
- *
+ * @c: variable
  * Return: void
  */
 int _isupper(int c)
 {
 	if (c >= 'A' && c <= 'Z')
-	{
 		return (1);
-	}
-	else 
-	{
+
 		return (0);
-	}
 }
