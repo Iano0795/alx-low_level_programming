1092575289712a78c24c2f0d574e5a3a30533e89
Cod3guy <ian.kipkorir0799@gmail.com>
2023-02-23 22:32:43 -0600

    More functions in c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks digits
+ * @c: variable
+ * Return: void
+ */
+int _isdigit(int c)
+{
+
+	if (c >= 0; c <= 9)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
