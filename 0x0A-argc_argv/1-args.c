1ec20a8fd196f81cba2bbbb18c58afe76eaa760c
Cod3guy <ian.kipkorir0799@gmal.com>
2023-03-11 09:07:17 -0800

    Msg--------->"Second task"

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - printing on arguments 
+ *
+ * @argc: shows the number of arguments supplied
+ * @argv: array of pointers
+ *
+ * Return: success(0)
+ */
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+
+	return (0);
+}
