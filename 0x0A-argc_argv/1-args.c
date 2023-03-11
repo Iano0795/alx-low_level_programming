462d192f6d2a9615faaf38c2a18be9ef7fb4d9cd
Cod3guy <ian.kipkorir0799@gmal.com>
2023-03-11 09:45:45 -0800

    Msg--------->"second task"

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -9,9 +9,10 @@
  *
  * Return: success(0)
  */
-int main(int argc, char *argv[])
+int main(int argc, char *argv[] __attribute__((unused)))
 {
-	printf("%d\n", argc);
+	if (argc > 0)
+		printf("%d\n", argc - 1);
 
 	return (0);
 }
