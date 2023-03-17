aab413b01eb2d58cc47c13511096dbfd5306ffa7
Cod3guy <ian.kipkorir0799@gmal.com>
2023-03-17 06:58:41 -0700

    Msg--------->"Changed the return type for malloc_checked function"

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -2,18 +2,14 @@
 
 /**
  * malloc_checked - checks for memory allocation
- *
  * @b: allocated memory
- *
  * Return: Returns memory allocated
  */
-
 void *malloc_checked(unsigned int b)
 {
 	void *i;
 
 	i = malloc(b);
-
 	if (i == NULL)
 	{
 		exit (98);
