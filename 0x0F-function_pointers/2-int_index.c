f947e84a19b1f0ed6754c88077635695e12d9c0a
Cod3guy <ian.kipkorir0799@gmal.com>
2023-03-22 04:30:38 -0700

    Msg--------->"Searching for an interger"

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * int_index - Searches for an interger
+ * @array: its the name of the array
+ * @size: is the number of elements in the array
+ * @cmp: function that compares the values in the array
+ * Return: 0 for success
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size <= 0)
+		return (-1);
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]))
+			return (i);
+	}
+	return (-1);
