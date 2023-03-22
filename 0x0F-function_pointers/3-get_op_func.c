3398a9138326b5b357e1d4cedc62f97254594f4d
Cod3guy <ian.kipkorir0799@gmal.com>
2023-03-22 11:51:50 -0700

    Msg--------->"made few changes"

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -20,9 +20,12 @@ int (*get_op_func(char *s))(int, int)
 
 	int i = 0;
 
-	for (; i < 5; i++)
+	while(i < 5)
+	{
 		if (strcmp(s, ops[i].op) == 0)
 			return (ops[i].f);
+		i++;
+	}
 
 	return (0);
 }
