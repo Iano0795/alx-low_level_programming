35e8e385fc8ebf98cbf690c22487d8a5da39c638
Cod3guy <ian.kipkorir0799@gmal.com>
2023-03-22 11:29:46 -0700

    Msg--------->"using pointers to functions as a switch case"

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,28 @@
+#include "3-calc.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * get_op_func - gets the operation functions
+ * @s: variable
+ * Return: returns a pointer to a function that corresponds to an operator
+ */
+int (*get_op_func(char *s)(int, int))
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+
+	int i = 0;
+
+	for (; i < 5; i++)
+		if (strcmp(s, ops[i].op) == 0)
+			return (ops[i].f);
+
+	return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,69 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * op_add - adds two numbers
+ * @a: int var
+ * @b; int var
+ * Return: the sum
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - Subtracting two numbers
+ * @a: int var
+ * @b: int var
+ * Return: the difference
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - multiply two numbers
+ * @a: int var
+ * @b: int var
+ * Return: the product
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - dividing two numbers
+ * @a: int var
+ * @b: int var
+ * Return: The dividend
+ */
+int op_div(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	return (a / b);
+}
+
+/**
+ * op_mod - gets the modulo between two numbers
+ * @a: int var
+ * @b: int var
+ * Return: the modulo
+ */
+int op_mod(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	return (a % b);
+}
+
