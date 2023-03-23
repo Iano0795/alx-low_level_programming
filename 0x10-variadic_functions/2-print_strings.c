686ac13c209d3bd7be011182a5cceedac0ca9774
Cod3guy <ian.kipkorir0799@gmal.com>
2023-03-23 06:03:18 -0700

    Msg--------->"last of all, and GOD said let my people go"

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -1,34 +1,33 @@
 #include "variadic_functions.h"
-#include <stdarg.h>
 #include <stdio.h>
+#include <stdarg.h>
 
 /**
- * print_strings - printing strings that are passed as arguments
- * @separator: character that separates the strings
- * @n: number of passed arguments
+ * print_strings - Prints strings passed to the function separed by separators
+ * @separator: A string that is used to separate strings
+ * @n: number of unnamed parameters
+ * Return: Always 0
  */
+
 void print_strings(const char *separator, const unsigned int n, ...)
 {
+	char *str;
 	unsigned int i;
 	va_list ap;
-	char *str;
 
 	va_start(ap, n);
-	if (separator != NULL)
-	{
-		for (i = 0; i < n; i++)
-		{
-			str = va_arg(ap, char*);
-			if (str != NULL)
-			{
-				printf("%s", str);
-				if (i != n - 1)
-					printf("%s ", separator);
-			}
-			else
-				printf("(nil)");
-		}
-	}
+	if (separator == NULL)
+		separator = "";
 
-	putchar('\n');
+	for (i = 0; i < n; i++)
+	{
+		str = va_arg(ap, char*);
+		if (str == NULL)
+			str = "(nil)";
+		printf("%s", str);
+		if (i < n - 1)
+			printf("%s", separator);
+	}
+	printf("\n");
+	va_end(ap);
 }
