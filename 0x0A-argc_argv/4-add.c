62b693808772c99fcacc6b633338d772ce5fb433
Iano0795 <iankipkorir668@gmail.com>
2023-06-14 05:31:12 -0700

    Challenge 4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -11,28 +11,26 @@
 
 int main(int argc, char *argv[])
 {
-	int i, n;
-	int count = argc;
-	int sum = 0;
-	char *flag;
+	int i, j, sum = 0;
 
 	if (argc < 2)
 	{
 		printf("%d\n", 0);
 	}
-	for (i = 1; i < count; i++)
+	for (i = 1; i < argc; i++)
 	{
-		n = strtol(argv[i], &flag, 10);
-		if (*flag)
+		for (j = 0; argv[i][j] != '\0'; j++)
 		{
-			printf("Error\n");
-			return (1);
+			if (argv[i][j] < '0' || argv[i][j] < '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
 		}
-		else
-		{
-			sum += n;
-		}
+
+		sum += atoi(argv[i]);
 	}
+
 	printf("%d\n", sum);
 	return (0);
 }
